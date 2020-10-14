1cee2fa165c3e48fe7c74d03c6728f23ef82f0c2
petroniuss <wojtyczekPatryk@gmail.com>
2020-10-14 21:51:20 +0200

    implemented rules in tokenizer

--- lab1/scanner.py
+++ lab1/scanner.py
@@ -1,10 +1,8 @@
-# ------------------------------------------------------------
-# calclex.py
-#
-# tokenizer for a simple expression evaluator for
-# numbers and +,-,*,/
-# ------------------------------------------------------------
 import ply.lex as lex
+
+# -------------------------------------------------------------
+# Module contains implementation of a tokenizer.
+# -------------------------------------------------------------
 
 # -------------------------------------------------------------
 # Literals
@@ -19,8 +17,6 @@ binaryOperators = [
 ]
 
 literals = binaryOperators + [
-    '<',  # Less than
-    '>',  # Greater than
     ':',  # Colon
     ',',  # Coma
     ';',  # Semicolon
@@ -29,9 +25,28 @@ literals = binaryOperators + [
     '{', '}',  # Curly  Brackets
 ]
 
+# -------------------------------------------------------------
+# Reserved Tokens
+# -------------------------------------------------------------
 
-# Todo
-# Make sure that strings are correctly defined!
+specialFunctons = {
+    'eye':      'EYE',
+    'zeros':    'ZEROS',
+    'ones':     'ONES',
+    'print':    'PRINT'
+}
+
+reserved = {
+    'if':       'IF',
+    'then':     'THEN',
+    'else':     'ELSE',
+    'for':      'FOR',
+    'while':    'WHILE',
+    'break':    'BREAK',
+    'continue': 'CONTINUE',
+    'return':   'RETURN',
+} + specialFunctons
+
 
 # -------------------------------------------------------------
 # Tokens
@@ -44,6 +59,11 @@ matrixBinaryOperations = (
     'DOTDIV'        # ./
 )
 
+matrixOperations = (
+    'TRANSPOSE',  # '
+    'SLICE',   # :
+)
+
 assignmentOperations = (
     'ASSIGN',  # =
     'SUBASSIGN',  # -=
@@ -52,60 +72,109 @@ assignmentOperations = (
     'MULTASSIGN'  # \ *=
 )
 
-
-# List of token names.   This is always required
-tokens = (
-    'INTNUM',       # int
-    'FLOATNUM',     # float
+relationalOperators = (
+    'LT',  # <
+    'GT',  # >
+    'LTE',  # <=
+    'GTE',  # >=
+    'EQL',  # ==
+    'NEQ',  # \ !=
 )
 
+numbers = (
+    'INTNUM',       # int
+    'FLOATNUM'     # float
+)
+
+strings = ('STR')
+
+identifier = ('ID')
+
+# List of token names.   This is always required
+tokens = \
+    matrixBinaryOperations +\
+    matrixOperations +\
+    assignmentOperations +\
+    relationalOperators +\
+    numbers +\
+    strings +\
+    identifier
+
 # -------------------------------------------------------------
-# Reserved Tokens
+# Matching Rules
 # -------------------------------------------------------------
-
-matrixReservedKeywords = {
-    'eye':      'EYE',
-    'zeros':    'ZEROS',
-    'ones':     'ONES'
-}
-reserved = {
-    'if':       'IF',
-    'then':     'THEN',
-    'else':     'ELSE',
-    'for':      'FOR',
-    'while':    'WHILE',
-    'break':    'BREAK',
-    'continue': 'CONTINUE',
-    'return':   'RETURN',
-    'print':    'PRINT'
-} + matrixReservedKeywords
+# Note:
+# - maximal is match is chosen (pattern which consumes the most characters)
+# - patterns appearing closer take precedence:
+#   - functions defined higher in this file,
+#   - strings are sorted based on pattern length.
 
 
-# A regular expression rule with some action code
+# Make sure that variables such as ___ are okay.
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value, 'ID')
+    return t
 
-def t_NUMBER(t):
-    r'\d+'
+
+t_TRANSPOSE = r'\''
+t_SLICE = r':',
+
+t_ASSIGN = r'='
+t_SUBASSIGN = r'-='
+t_ADDASSGIN = r'+='
+t_DIVASSIGN = r'/=',
+t_MULTASSIGN = r'*='
+
+t_LT = r'<'
+t_GT = r'>'
+t_LTE = r'<='
+t_GTE = r'>='
+t_EQL = r'=='
+t_NEQ = r'!='
+
+
+def t_STR(t):
+    # Todo
+    # Make sure that strings are correctly defined!
+    # This one might cause problems
+    r'".*"'
+    t.value = t.value[1: len(t.value) - 1]
+    return t
+
+
+def t_INTNUM(t):
+    r'[-+]?(\d+)'  # It allows for leading zeros.
     t.value = int(t.value)
     return t
 
-# Define a rule so we can track line numbers
+
+def t_FLOATNUM(t):
+    # It also allows for leading zeros and for scientific notation.
+    r'[-+]?(\d+(\.\d*)|\.\d+)([eE][-+]?\d+)?'
+    t.value = float(t.value)
+    return t
 
 
 def t_newline(t):
+    # Define a rule so we can track line numbers
     r'\n+'
     t.lexer.lineno += len(t.value)
 
 
-# A string containing ignored characters (spaces and tabs)
-t_ignore = ' \t'
-
-# Error handling rule
+def t_COMMENT(t):
+    # This one needs to be placed at the bottom so that it does not interfere with others!
+    r'#.*'
+    pass
 
 
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore = ' \t'
 
 # Build the lexer
 lexer = lex.lex()
