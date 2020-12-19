3090ac81d1a36b9841bfece5b04a95ecb0decc32
petroniuss <wojtyczekPatryk@gmail.com>
2020-12-19 23:28:05 +0100

    code generator test pipeline

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from Failure import CompilationFailure
 
 def readFile():
     try:
-        filename = sys.argv[1] if len(sys.argv) > 1 else "./tests/example1.m"
+        filename = sys.argv[1] if len(sys.argv) > 1 else "./tests/ir/foo.m"
         file = open(filename, "r")
     except IOError:
         print("Cannot open {0} file".format(filename))
